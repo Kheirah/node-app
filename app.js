a9c55162ec53ee05885a59b141da2d9dabad32ad
Ryan Ceres <c.nizz@web.de>
2022-10-21 11:03:03 +0200

    add new route with shoe data

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ app.get("/", (req, res) => {
   res.json({ message: "express on vercel: it works!" });
 });
 
+app.get("/shoes", (req, res) => {
+  res.json({
+    name: "Nike Air Jordan",
+    color: ["white", "red", "grey"],
+    price: 120,
+  });
+});
+
 app.listen(port, "localhost", () => {
   console.log(`Node app listening at http://localhost:${port}`);
 });
